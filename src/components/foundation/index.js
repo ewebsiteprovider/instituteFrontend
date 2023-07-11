f3601b7a6e45fe9c80f82465635d771d7e7ee3f1
AmritHimanshu <amrit.ewp@gmail.com>
2023-07-11 23:19:54 +0530

    made required changes

--- src/components/foundation/index.js
+++ src/components/foundation/index.js
@@ -13,7 +13,7 @@ function Foundation() {
       }}>
         <div className="px-10 w-full h-full flex flex-col items-center justify-center bg-yellow-10 bg-opacity-30 ">
           <div>
-            <div className="py-5 md:py-14 text-[30px] sm:text-[40px] md:text-[50px] font-bold sm:leading-[70px] text-[red] font-bold underline">
+            <div className="py-5 md:py-2 text-[30px] sm:text-[40px] md:text-[50px] font-bold sm:leading-[70px] text-[red] font-bold px-3 bg-white bg-opacity-80 rounded-xl underline">
               PATNA SCIENCE ACADEMY PROGRAM DETAILS
             </div>
 
