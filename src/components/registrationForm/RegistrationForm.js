e01f667faf1ef3519e90d854648471ab967e52c0
AmritHimanshu <amrit.ewp@gmail.com>
2023-07-22 01:06:11 +0530

    fixed mobile field in registration form

--- src/components/registrationForm/RegistrationForm.js
+++ src/components/registrationForm/RegistrationForm.js
@@ -66,7 +66,7 @@ const RegistrationForm = () => {
 
             <div className="flex flex-col space-y-2 w-[250px] sm:w-[400px] md:w-[600px]">
               <label className='text-white'>Mobile <span className='text-red-500'>*</span></label>
-            <input className="px-2 sm:px-5 py-1 sm:py-2 text-black text-md rounded-lg sm:rounded-xl outline-0 placeholder-gray-400 placeholder:italic placeholder:text-sm" type="tel" pattern="[0-9]{10}" value={mobile} onChange={handleMobileChange} placeholder='Your Mobile Number' required />
+            <input className="px-2 sm:px-5 py-1 sm:py-2 text-black text-md rounded-lg sm:rounded-xl outline-0 placeholder-gray-400 placeholder:italic placeholder:text-sm" type="tel" pattern="[7-9]{1}[0-9]{9}" value={mobile} onChange={handleMobileChange} placeholder='Your Mobile Number' required />
             </div>
 
             <div className="flex flex-col space-y-2 w-[250px] sm:w-[400px] md:w-[600px]">
